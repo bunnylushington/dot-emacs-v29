3652227fcc35ac03a6a955ca88a819a10453458b
Bunny Lushington <bunny@bapi.us>
2023-08-08 11:21:27 -0400

    integrate bookmarks with eshell

--- init.el
+++ init.el
@@ -1479,8 +1479,6 @@ that we can generate a skeleton with the cobracmd yasnippet."
       t)))
 
 
-
-
 ;; This audio control stuff requires:
 ;;   brew install switchaudio-osx
 ;;   brew tap meowmeowmeowcat/taps
@@ -1507,3 +1505,47 @@ that we can generate a skeleton with the cobracmd yasnippet."
       (let ((record (split-string src ",")))
         (add-to-list 'outputs (car record))))
     outputs))
+
+
+;; eshell/bmk - version 0.1.3
+;;
+;; See https://www.emacswiki.org/emacs/EshellBmk
+;;
+;; Note that the nano theme writes bookmarks (and other session mgmt
+;; stuff) to ~/.nano-bookmarks.  I tend to forget this.
+
+(defun pcomplete/eshell-mode/bmk ()
+  "Completion for `bmk'"
+  (pcomplete-here (bookmark-all-names)))
+
+(defun eshell/bmk (&rest args)
+  "Integration between EShell and bookmarks.
+For usage, execute without arguments."
+  (setq args (eshell-flatten-list args))
+  (let ((bookmark (car args))
+        filename name)
+    (cond
+     ((eq nil args)
+      (format "Usage:
+* bmk BOOKMARK to
+** either change directory pointed to by BOOKMARK
+** or bookmark-jump to the BOOKMARK if it is not a directory.
+* bmk . BOOKMARK to bookmark current directory in BOOKMARK.
+Completion is available."))
+     ((string= "." bookmark)
+      ;; Store current path in EShell as a bookmark
+      (if (setq name (car (cdr args)))
+          (progn
+            (bookmark-set name)
+            (bookmark-set-filename name (eshell/pwd))
+            (format "Saved current directory in bookmark %s" name))
+        (error "You must enter a bookmark name")))
+     (t
+       ;; Check whether an existing bookmark has been specified
+       (if (setq filename (bookmark-get-filename bookmark))
+           ;; If it points to a directory, change to it.
+           (if (file-directory-p filename)
+               (eshell/cd filename)
+             ;; otherwise, just jump to the bookmark
+             (bookmark-jump bookmark))
+         (error "%s is not a bookmark" bookmark))))))
