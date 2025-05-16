932d832bd1e8fef74078c776f230ee3454037566
Bunny Lushington <bunny@bapi.us>
2025-05-16 16:01:40 +0700

    feat: Enhance recentf project file opening and enable mode
    - Enabled `recentf-mode` by default.
    - Bound `C-c r` to `ii/project-recentf-open` for quick access.
    - Modified `ii/project-recentf-open`:
        - It now prioritizes suggesting files from the current project but falls back to all recent files, rather than being strictly limited to project files.
        - The prompt and docstring were updated to reflect this broader scope.
    - Improved `ii/project-recentf-open-files` to notify the user if no recent files are found within the current project.

--- config/ii-recentf.el
+++ config/ii-recentf.el
@@ -1,15 +1,16 @@
+(recentf-mode 1)
 (customize-set-value 'recentf-max-saved-items 500)
 
+(global-set-key (kbd "C-c r") #'ii/project-recentf-open)
+
 (defun ii/project-recentf-open ()
-  "Like `recentf-open' but limited to current project"
+  "Like `recentf-open' but try current project files first."
   (interactive)
-  (if (project-current)
-      (let ((file (completing-read
-                   (format-prompt "Open recent project file" nil)
-                   (ii/recentf-filter-current-project) nil t)))
-        (when file
-          (funcall recentf-menu-action file)))
-    (message "No current project found")))
+  (let ((file (completing-read
+               (format-prompt "Open recent file" nil)
+               (ii/recentf-current-project-files) nil t)))
+    (when file
+      (funcall recentf-menu-action file))))
 
 (defun ii/project-recentf-open-files (&optional dir)
   "Like `recentf-open-files' but limited to current project"
@@ -17,8 +18,10 @@
   (let ((target-directory (or dir default-directory))
         (proj (project-current nil (or dir default-directory))))
     (if proj
-        (recentf-open-files
-         (ii/recentf-current-project-files proj))
+        (let ((files (ii/recentf-current-project-files proj)))
+          (if files
+              (recentf-open-files files)
+            (message "No recent project files found.")))
       (message "No current project found."))))
 
 (defun ii/recentf-current-project-files (&optional project)
